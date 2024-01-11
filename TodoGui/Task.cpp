f0c70825b687737d9a66965bdf5adaa94d73c1e0
Alvin Yap Abidin <alvin.z.y.a@gmail.com>
2024-01-11 20:44:34 +1100

    Add project files.

---
+++ TodoGui/Task.cpp
@@ -0,0 +1,25 @@
+#include "Task.h"
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+
+void saveTasksToFile(const std::vector<Task>& tasks, const std::string& fileName)
+{
+    std::ofstream ostream(fileName);
+    ostream << tasks.size();
+
+    for (const Task& task : tasks)
+    {
+        std::string description = task.description;
+        std::replace(description.begin(), description.end(), ' ', '_');
+
+        ostream << '\n' << description << ' ' << task.done;
+    }
+}
+
+std::vector<Task> loadTasksFromFile(const std::string& fileName)
+{
+    return std::vector<Task>();
+}
