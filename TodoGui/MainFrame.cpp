c408eac93f7eab5b3edde35c98f14a7077199e1e
Alvin Yap Abidin <alvin.z.y.a@gmail.com>
2024-01-11 21:03:16 +1100

    Created application with title

---
+++ TodoGui/MainFrame.cpp
@@ -0,0 +1,12 @@
+// Credit goes to OttoBotCode for the tutorial, thank you for helping me learn C++
+
+#include "MainFrame.h"
+#include <wx/wx.h>
+#include <vector>
+#include <string>
+#include "Task.h"
+
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
+{
+
+}
